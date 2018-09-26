899544967295d1b996b8638abf2913767fdb9278
justathoughtor2 <thoughthaven@gmail.com>
2018-09-26 19:33:44 -0400

    Switch to scheduled execution of image update

--- main.py
+++ main.py
@@ -9,22 +9,33 @@ from requests_html import HTMLSession
 
 from PIL import Image, ImageDraw, ImageFont
 
-session = HTMLSession()
+import time
+import atexit
+
+from appschduler.schedulers.background import BackgroundScheduler
 
 app = Flask(__name__)
 
+output = BytesIO()
 
-@app.route('/')
-def hello():
-    r = session.get('https://en.wikiquote.org/wiki/Wikiquote:Quote_of_the_day?action=render')
+generate_image()
+
+scheduler = BackgroundScheduler(timezone=utc)
+scheduler.add_job(func=generate_image, trigger='cron', hour=0, minute=5)
+
+def generate_image():
+	session = HTMLSession()
+	r = session.get('https://en.wikiquote.org/wiki/Wikiquote:Quote_of_the_day?action=render')
     wikitext = r.html.find('div > center > table', first=True).text
     img = Image.new('RGB', (800,150), color='white')
     fnt = ImageFont.truetype('Montserrat-Regular.ttf', 14)
     d = ImageDraw.Draw(img)
     d.multiline_text((10,10), wikitext, font=fnt, fill='black', spacing=6)
-    output = BytesIO()
     img.save(output, format='PNG')
     output.seek(0)
+
+@app.route('/')
+def hello():
     return send_file(output, mimetype='image/png')
 
 @app.errorhandler(500)
