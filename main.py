6966f30180341136edebc1aecac4d7732e956f35
justathoughtor2 <thoughthaven@gmail.com>
2018-09-26 22:22:07 -0400

    Move session variable to route

--- main.py
+++ main.py
@@ -9,16 +9,13 @@ from requests_html import HTMLSession
 
 from PIL import Image, ImageDraw, ImageFont
 
-session = HTMLSession()
-
 app = Flask(__name__)
-
 
 @app.route('/')
 def hello():
+    session = HTMLSession()
     r = session.get('https://en.wikiquote.org/wiki/Wikiquote:Quote_of_the_day?action=render')
     wikitext = r.html.find('div > center > table', first=True).text
-    logging.info(wikitext)
     img = Image.new('RGB', (800,150), color='white')
     fnt = ImageFont.truetype('Montserrat-Regular.ttf', 14)
     d = ImageDraw.Draw(img)
@@ -26,11 +23,9 @@ def hello():
     output = BytesIO()
     img.save(output, format='PNG')
     output.seek(0)
-    return send_file(output, mimetype='image/png')
+    return send_file(output, mimetype='image/png'), 200
 
 @app.errorhandler(500)
 def server_error(e):
-    # Log the error and stacktrace.
     logging.exception('An error occurred during a request.')
     return 'An internal error occurred.', 500
-# [END app]wg
